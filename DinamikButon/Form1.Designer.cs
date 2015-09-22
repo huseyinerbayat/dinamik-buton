0bad35042fc7b82243221c9ad3d699555ddf35a6
huseyinerbayat <huseyin_erbayat@hotmail.com>
2015-09-22 20:03:44 +0300

    Dinamik Buton Olusturma

---
+++ DinamikButon/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace DinamikButon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._btnButonOlustur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _btnButonOlustur
+            // 
+            this._btnButonOlustur.Location = new System.Drawing.Point(97, 72);
+            this._btnButonOlustur.Name = "_btnButonOlustur";
+            this._btnButonOlustur.Size = new System.Drawing.Size(90, 23);
+            this._btnButonOlustur.TabIndex = 0;
+            this._btnButonOlustur.Text = "Buton Oluştur";
+            this._btnButonOlustur.UseVisualStyleBackColor = true;
+            this._btnButonOlustur.Click += new System.EventHandler(this._btnButonOlustur_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 357);
+            this.Controls.Add(this._btnButonOlustur);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _btnButonOlustur;
+    }
+}
+
---
+++ DinamikButon/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DinamikButon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _btnButonOlustur_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button(); // butonun oluşturulması
+            btn.Location = new Point(10, 100); // butonun koordinatları
+            btn.Height = 40; // butonun yüksekliği
+            btn.Width = 300; // butonun genişliği
+            btn.Text = "Yeni Oluşan Buton"; // butonun texti
+            btn.Click += new EventHandler(btn_Click); // butonun eventi
+            btn.BackColor = Color.Turquoise; // butonun rengi
+            
+            Controls.Add((Button)btn); // butonun eklenmesi
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ben yeni oluşan butonum!","Yeni Buton Mesaj");
+        }
+    }
+}
